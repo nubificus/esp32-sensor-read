e3236b1ebc79891e95f29f129fb47c36e6a28b70
Ilias Lagomatis <ilago@nubificus.co.uk>
2024-11-14 11:27:22 +0200

    feat: http_server: Cleanup files
    We now need only the request handlers,
    therefore server initialization code and
    URI's were deleted.
    
    PR: https://github.com/nubificus/esp32-ota-update/pull/7
    Signed-off-by: Ilias Lagomatis <ilago@nubificus.co.uk>
    Reviewed-by: Ilias Lagomatis <ilago@nubificus.co.uk>
    Approved-by: Ilias Lagomatis <ilago@nubificus.co.uk>

--- main/http_server.h
+++ main/http_server.h
@@ -6,12 +6,4 @@
 esp_err_t info_get_handler(httpd_req_t *req);
 esp_err_t temp_get_handler(httpd_req_t *req);
 
-httpd_handle_t start_webserver(void);
-void stop_webserver(httpd_handle_t server);
-
-void disconnect_handler(void* arg, esp_event_base_t event_base,
-                        int32_t event_id, void* event_data);
-void connect_handler(void* arg, esp_event_base_t event_base,
-                     int32_t event_id, void* event_data);
-
 #endif // HTTP_SERVER_H
